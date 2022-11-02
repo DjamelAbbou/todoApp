f6dd99ac7def21f3ac88e9813ebfd5e1160be9a9
djamelabbou <djamelabbou@outlook.com>
2022-11-03 00:10:11 +0100

    testing out github

---
+++ src/pages/index.jsx
@@ -0,0 +1,32 @@
+import { getUsers } from "@/api/users";
+import { jsonify } from "@/modules/db";
+import Container from "@/ui/container";
+
+export default function HomePage({ users = [] }) {
+  return (
+    <Container className="mt-16 text-white text-2xl flex flex-col gap-8">
+      Hello world
+    </Container>
+  );
+}
+
+export async function getServerSideProps({ ctx }) {
+  const users = await getUsers();
+
+  return {
+    props: {
+      users: jsonify(users),
+    },
+  };
+}
+// export function withValidations(Component) {
+//   return function wrappedComponent({}) {
+//     const [errors, setErrors] = useState([]);
+//     return (
+//       <>
+//         <Component />
+//         {errors.length > 0 && <div>{errors.join(", ")}</div>}
+//       </>
+//     );
+//   };
+// }
